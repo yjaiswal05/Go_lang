c476d7d0abeea1539793736d4bd14e9755052d4b
Yash Jaiswal <yjaiswal0511@gmail.com>
2024-08-07 20:54:07 +0530

    Update main.go

--- Projects/1-Slack_Age_bot/main.go
+++ Projects/1-Slack_Age_bot/main.go
@@ -14,8 +14,8 @@ func main() {
 	
 	//Setting environment
 	//These tokens authenticate the bot with Slack.
-	os.Setenv("SLACK_BOT_TOKEN","<Slack bot token>")
-	os.Setenv("SLACK_APP_TOKEN","<Slack app token>")
+	os.Setenv("SLACK_BOT_TOKEN","<Your Slack bot token>")
+	os.Setenv("SLACK_APP_TOKEN","<Your Slack app token>")
 
 	//creating bot
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"),os.Getenv("SLACK_APP_TOKEN"))
