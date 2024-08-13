6da21fc61d737a07b9fc84d0f99d8b24d1b4d53b
Yash Jaiswal <yjaiswal0511@gmail.com>
2024-08-13 14:22:57 +0530

    Easy

--- Projects/4-AWS_Lambda/go.mod
+++ Projects/4-AWS_Lambda/go.mod
@@ -1,3 +1,5 @@
 module lambda
 
 go 1.22.5
+
+require github.com/aws/aws-lambda-go v1.47.0 // indirect
--- Projects/4-AWS_Lambda/main.go
+++ Projects/4-AWS_Lambda/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main()  {
-	
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(HandleLambdaEvent)
+}
+
+type Myevent struct{
+	Name string 	`json: "what is you name?"`
+	Age int 		`json:"How old are you?"`
+}
+
+type MyResponse struct{
+	Message string	`json:"Answer"`
+}
+
+func HandleLambdaEvent(event Myevent)(MyResponse, error)  {
+	return MyResponse{Message: fmt.Sprintf("%s is %d years old!",event.Name,event.Age)},nil
 }
