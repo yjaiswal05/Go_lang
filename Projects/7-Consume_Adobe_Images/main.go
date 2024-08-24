12500b889c977e9f7fb9a50cdc03341c34d7aa84
Yash Jaiswal <yjaiswal0511@gmail.com>
2024-08-24 20:53:30 +0530

    Medium

--- Projects/7-Consume_Adobe_Images/main.go
+++ Projects/7-Consume_Adobe_Images/main.go
@@ -12,11 +12,11 @@ import (
 )
 
 func main() {
-	os.Setenv("PexelsToken", "")
+	os.Setenv("PexelsToken", "s1cmhRgBCgxMS6tWPP5q5GfTtxXVjX22aPNIwoA1CDlR7cmaXwn63aQb")
 	var TOKEN = os.Getenv("PexelsToken")
 	
 	var c = NewClient(TOKEN)
-	result, err := c.searchPhotos("sky",5,1)
+	result, err := c.SearchPhotos("sky",5,1)
 	if err != nil {
 		fmt.Errorf("Search error:%v", err)
 	}
@@ -27,8 +27,8 @@ func main() {
 }
 
 const (
-	PhotoApi = ""
-	VideoApi = ""
+	PhotoApi = "https://api.pexels.com/v1/"
+	//VideoApi = "https://api.pexels.com/videos/"
 )
 
 type Client struct{
@@ -43,31 +43,36 @@ func NewClient(token string) *Client{
 }
 
 type SeachResult struct{
-	Page 			int32
-	PerPage 		int32
-	TotalResult 	int32
-	NextPage 		string
-	Photos 			[]Photo
+	Page 			int32			`json:"page"`
+	PerPage 		int32			`json:"per_page"`
+	TotalResults 	int32			`json:"total_Results"`
+	NextPage 		string			`json:"next_page"`
+	Photos 			[]Photo			`json:"photos"`
 }
 
 type Photo struct{
-	Id				int32
-	Width			int32
-	Height			int32
-	Url				string
-	Photographer	string
-	Src				PhotoSource
+	Id				int32			`json:"id"`
+	Width			int32			`json:"width"`
+	Height			int32			`json:"height"`
+	Url				string			`json:"url"`
+	Photographer	string			`json:"photographer"`
+	Src				PhotoSource		`json:"src"`
 }
 
 type PhotoSource struct{
-	Original		string
-	Large			string
-	Medium			string
-	Small			string
-	Portrait		string
+	Original		string			`json:"original"`
+	Large			string			`json:"large"`
+	Medium			string			`json:"medium"`
+	Small			string			`json:"small"`
+	Portrait		string			`json:"portrait"`
 }
-
-func (c *Client) searchPhotos(query string, perPage, page int) (*SeachResult,error)  {
+type CuratedResult struct{
+	Page			int32			`json:"page"`
+	PerPage			int32			`json:"per_page"`
+	NextPage		int32			`json:"next_page"`
+	Photos			[]Photo			`json:"photos"`
+}
+func (c *Client) SearchPhotos(query string, perPage, page int) (*SeachResult,error)  {
 	url := fmt.Sprintf(PhotoApi+"/search?query=%s&per_page=%d&page=%d",query,perPage,page)
 	resp , err := c.requestDoWithAuth("GET",url)
 	defer resp.Body.Close()
@@ -99,3 +104,15 @@ func (c *Client) requestDoWithAuth(method, url string) (*http.Response,error)  {
 	return resp,nil
 }
 
+func (c *Client) CuratedPhotos(perPage, page int) (*CuratedResult,error)  {
+	url := fmt.Sprintf(PhotoApi+"/curated?per_page=%d&page=%d",perPage,page)
+	resp , err := c.requestDoWithAuth("GET",url)
+	defer resp.Body.Close()
+	data,err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil,err
+	}
+	var result CuratedResult
+	err = json.Unmarshal(data, &result)
+	return &result,err
+}
